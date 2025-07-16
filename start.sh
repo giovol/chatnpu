4525e81ed0a4a16b937901837010bf651ab42830
giovol <220977411+giovol@users.noreply.github.com>
2025-07-16 21:41:18 +0200

    Updated start script

--- start.sh
+++ start.sh
@@ -1,23 +1,11 @@
 echo "Welcome to ChatNPU!"
 
-# Clone the repository of open model zoo, and if already exists update it
-DIR=./open_model_zoo
-if [ -d "$DIR" ];
-then
-    echo "$DIR already exists. Pulling the latest version..."
-    cd ./open_model_zoo
-    git pull
-    cd ..
-else
-	echo "$DIR directory does not exist. Cloning the repository..."
-    git clone https://github.com/openvinotoolkit/open_model_zoo.git
-fi
-
 # Setup OpenVINO env
-python3 -m venv
-source .venv/bin/activate
-python -m pip install --upgrade pip
-pip install openvino-dev
+echo "Setting up OpenVINO venv..."
+python3 -m venv .venv > /dev/null
+source .venv/bin/activate > /dev/null
+python -m pip install --upgrade pip > /dev/null
+pip install openvino > /dev/null
 
 read -r -p "Do you want to opt out from OpenVINO™ telemetry? [y/N] " response
 case "$response" in
