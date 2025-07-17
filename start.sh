a824f668c11fe93ae8e773136999358665cbb8fc
giovol <220977411+giovol@users.noreply.github.com>
2025-07-17 10:19:11 +0200

    Implement telemetry choice

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo "Welcome to ChatNPU!"
 
 # Setup OpenVINO env
@@ -7,12 +9,20 @@ source .venv/bin/activate > /dev/null
 python -m pip install --upgrade pip > /dev/null
 pip install openvino > /dev/null
 
-read -r -p "Do you want to opt out from OpenVINO™ telemetry? [y/N] " response
-case "$response" in
-    [yY][eE][sS]|[yY]) 
-        opt_in_out --opt_out
-        ;;
-    *)
-        opt_in_out --opt_in
-        ;;
-esac
+# Check if telemetry file exists and, if not, ask the user to make a choice
+TELEMETRYFILE=~/intel/openvino_telemetry
+if [ -f "$TELEMETRYFILE" ]; then
+    echo -n "OpenVINO™ telemetry is set to " && cat "$TELEMETRYFILE"
+    echo 
+    echo "To make the choice again, delete this file: ~/intel/openvino_telemetry"
+else 
+    read -r -p "Do you want to opt out from OpenVINO™ telemetry? [y/N] " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            opt_in_out --opt_out
+            ;;
+        *)
+            opt_in_out --opt_in
+            ;;
+    esac
+fi
