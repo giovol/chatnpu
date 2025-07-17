2cd58a8e9bc45bc750be66853f67f99cdaa1909f
giovol <220977411+giovol@users.noreply.github.com>
2025-07-17 19:38:38 +0200

    Finish start shell script

--- start.sh
+++ start.sh
@@ -2,50 +2,63 @@
 
 echo "Welcome to ChatNPU!"
 
-# Ask if the user wants to setup OpenVINO venv
-VENV=./.venv
-setupvenv()
-{
+# Function to setup venv
+setupvenv() {
     echo "Setting up OpenVINO™ venv..."
     python3 -m venv .venv
-    source .venv/bin/activate
+    . .venv/bin/activate
     python -m pip install --upgrade pip
     pip install --upgrade openvino==2025.2.0
     pip install --upgrade flask
     pip install --upgrade "optimum[openvino]"
 }
 
-if [ -f "$VENV" ]; then
-    read ".venv directory already exists. Do you want to update .venv? [y/N] " response1
-    case "$response1" in
-        [yY][eE][sS]|[yY]) 
+# Ask to the user if they want to setup venv
+VENV=./.venv
+if [ -d "$VENV" ]; then
+    read -r -p ".venv directory already exists. Do you want to update .venv? [y/N] " venvchoice
+    case "$venvchoice" in
+        [yY][eE][sS]|[yY])
             setupvenv
-            ;;
+        ;;
         *)
-            ;;
+        ;;
     esac
 else
     setupvenv
 fi
 
-# Check if telemetry file exists and, if not, ask the user to make a choice
+# Ask to the user if they want to disable telemetry
 TELEMETRYFILE=~/intel/openvino_telemetry
 if [ -f "$TELEMETRYFILE" ]; then
     echo -n "OpenVINO™ telemetry is set to " && cat "$TELEMETRYFILE"
-    echo 
+    echo
     echo "To make the choice again, delete this file: ~/intel/openvino_telemetry"
-else 
-    read -r -p "Do you want to opt out from OpenVINO™ telemetry? [y/N] " response2
-    case "$response2" in
-        [yY][eE][sS]|[yY]) 
+else
+    read -r -p "Do you want to opt out from OpenVINO™ telemetry? [y/N] " telemetrychoice
+    case "$telemetrychoice" in
+        [yY][eE][sS]|[yY])
             opt_in_out --opt_out
-            ;;
+        ;;
         *)
             opt_in_out --opt_in
-            ;;
+        ;;
     esac
 fi
 
-optimum-cli export openvino --model openai-community/gpt2 --task text-generation-with-past --weight-format int8 neural-chat/INT8
+# Ask to the user which model do they want to use, and if the user doesn't know automatically choose Intel/neural-chat-7b-v3-3
+# You can find models at https://huggingface.co
+MODEL=./neural-chat/
+if [ -d "$MODEL" ]; then
+    print "Model found! If you want to use another one, please delete the 'neural-chat' directory on the script folder"
+else
+    read -r -p "Which model do you want to use? If you're unsure leave blank: " model
+    if [ -n "$model" ]; then
+        :
+    else
+        model="Intel/neural-chat-7b-v3-3"
+    fi
+    optimum-cli export openvino --model "$model" --task text-generation-with-past --weight-format int8 neural-chat/INT8
+fi
 
 python ./app.py
